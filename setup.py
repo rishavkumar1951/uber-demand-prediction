f52867710cff7ca6813f7a56df6ae068932dcbc1
Rishav Kumar <rishavkumar1951@gmail.com>
2025-05-29 22:56:34 +0530

    Added DVC pipeline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A ML model which can predict customer cab demand for a region at a particular time to Cab driver.',
+    author='Rishav',
+    license='MIT',
+)
